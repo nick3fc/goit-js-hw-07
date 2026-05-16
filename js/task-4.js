6dea3b3a21cb3444ae63429e48190a3e48a4b036
nick3fc <nick3fc@gmail.com>
2026-05-16 15:37:16 +0300

    hw07-1

--- js/task-4.js
+++ js/task-4.js
@@ -3,7 +3,10 @@ const form = document.querySelector('.login-form');
 form.addEventListener('submit', event => {
   event.preventDefault();
 
-  if (form.elements.email.value !== '' || form.elements.password.value !== '') {
+  if (
+    form.elements.email.value.trim() !== '' &&
+    form.elements.password.value.trim() !== ''
+  ) {
     const formData = {
       [form.elements.email.name]: form.elements.email.value.trim(),
       [form.elements.password.name]: form.elements.password.value.trim(),
